50fc8b3e9ecc883b8b0c440602b0f914cb714b9a
James Mata <jamesmata556@gmail.com>
2023-04-12 17:09:34 -0400

    adding test environment

---
+++ TestEnvironment/GetRoomList/src/main/java/com/example/getroomlist/GetRoomListApplication.java
@@ -0,0 +1,9 @@
+package com.example.getroomlist;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class GetRoomListApplication extends Application {
+
+}
