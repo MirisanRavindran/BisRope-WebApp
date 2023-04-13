81d2bb7fb1a6f7f50d44fd8d639e4154eff88a40
Harsh <harshpanchal30x@gmail.com>
2023-04-13 11:48:28 -0400

    images now show

--- TestEnvironment/Test - Harsh/ChatServer&Client/src/main/webapp/js/main.js
+++ TestEnvironment/Test - Harsh/ChatServer&Client/src/main/webapp/js/main.js
@@ -76,15 +76,14 @@ function newRoom(){
             // Set up an event listener for incoming messages
             ws.onmessage = function (event) {
                 console.log(event.data);
-                // Parse the incoming message and append it to the chat log on the web page
                 let message = JSON.parse(event.data);
-                document.getElementById("log").value += "[" + timestamp() + "] " + message.message + "\n";
+                document.getElementById("log").innerHTML += "[" + timestamp() + "] " + message.message + "<br>";
             }
-            document.getElementById("input").addEventListener("keyup", function (event) {
+            document.getElementById("input").addEventListener("keydown", function (event) {
                 if (event.keyCode === 13) {
-                    let request = {"type":"chat", "msg":event.target.value};
+                    let request = {"type":"chat", "msg":event.target.textContent};
                     ws.send(JSON.stringify(request));
-                    event.target.value = "";
+                    event.target.textContent = "";
                 }
             });
         })
@@ -103,23 +102,43 @@ function enterRoom(code) {
     ws.onmessage = function (event) {
         console.log(event.data);
         let message = JSON.parse(event.data);
-        document.getElementById("log").value += "[" + timestamp() + "] " + message.message + "\n";
+        document.getElementById("log").innerHTML += "[" + timestamp() + "] " + message.message + "<br>";
     }
-    document.getElementById("input").addEventListener("keyup", function (event) {
+    document.getElementById("input").addEventListener("keydown", function (event) {
         if (event.keyCode === 13) {
-            let request = {"type":"chat", "msg":event.target.value};
+            let request = {"type":"chat", "msg":event.target.textContent};
             ws.send(JSON.stringify(request));
-            event.target.value = "";
+            event.target.textContent = "";
         }
     });
 }
 
+function ChooseImage() {
+    document.getElementById('imageFile').click();
+}
+function SendImage(event) {
+    var file = event.files[0];
 
+    if (!file.type.match("image.*")) {
+        alert("Please select image only.");
+    }
+    else {
+        var reader = new FileReader();
+
+        reader.addEventListener("load", function(){
+                let message = reader.result;
+                message = `<img src='${message}' class="img-fluid"/>`;
+                document.getElementById("log").innerHTML += "[" + timestamp() + "] " + message + "\n";
+            }
+            , false);
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+}
 function timestamp() {
     var d = new Date(), minutes = d.getMinutes();
     if (minutes < 10) minutes = '0' + minutes;
     return d.getHours() + ':' + minutes;
 }
-
-
-
