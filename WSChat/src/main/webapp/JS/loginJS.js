89e79e4fc36dde5221bb1c29a14659a441e19ba2
Dennis Martin <dennis.martin.1010@outlook.com>
2023-04-14 19:04:01 -0400

    Got homepageJS to work

--- WSChat/src/main/webapp/JS/loginJS.js
+++ WSChat/src/main/webapp/JS/loginJS.js
@@ -27,11 +27,38 @@ function createUser() {
             }
             throw new Error('Network response was not ok.');
         })
- 
-
         //Creates a message in HTML that indicates that the account has already been created
-
-    
-
 }
 
+
+
+function logIn(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    const url = "http://localhost:8080/BisRopeServer-1.0-SNAPSHOT/api/bisrope-server/login/"+ username +"/" + password;
+    
+
+    fetch(url, {
+        method: 'GET',
+    })
+        .then(response => {
+            if (response.ok) {
+                // If the response status is "ok", return the response text
+                console.log(response.text);
+                alert("Login successful");
+                window.location.href = "http://localhost:8080/WSChat/chat.html";
+            }
+            if (response.status === 401){
+                console.log("Username or password is incorrect. Please try again.");
+                alert("Username or password is incorrect. Please try again.");
+
+            }
+            throw new Error('Network response was not ok.');
+        })
+
+    //pass the username variable to the homepageJS file
+    localStorage.setItem("username", username);
+    console.log(localStorage.getItem("username"));
+
+        
+}
