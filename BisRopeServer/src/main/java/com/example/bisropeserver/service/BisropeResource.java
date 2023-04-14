138a6f05dc5f311cdce70ef93aed2bf8df1e05b7
James Mata <jamesmata556@gmail.com>
2023-04-14 16:30:31 -0400

    more resource functions

--- BisRopeServer/src/main/java/com/example/bisropeserver/service/BisropeResource.java
+++ BisRopeServer/src/main/java/com/example/bisropeserver/service/BisropeResource.java
@@ -2,13 +2,112 @@ package com.example.bisropeserver.service;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Post;
 import jakarta.ws.rs.Produces;
+import com.example.bisropeserver.*;
+import com.example.bisropeserver.util.BisropeServers;
+import com.example.bisropeserver.util.BisropeUsers;;
 
-@Path("/hello-world")
+@Path("/bisrope-server")
 public class BisropeResource {
+
+    static BisropeUsers users = new BisropeUsers();
+    static BisropeServers servers = new BisropeServers();
+
+
+    @POST
+    @Produces("text/plain")
+    @Path("/create-account/{username}/{password}")
+    public Response createAccount(@PathParam("username") String username, @PathParam("password") String password) throws Exception {
+        // Check if email is already in use
+        if (users.isUser(username)) {
+            return Response.status(401).entity("Username already in use.").build();
+        }
+    
+        // Create new user
+        users.createUser(username, password);
+    
+        // Return success response
+        return Response.ok("User created successfully.").build();
+    }
+
+    @GET
+    @Path("/login/{username}/{password}")
+    @Produces("text/plain")
+    public Response login(@PathParam("username") String username, @PathParam("password") String password) {
+        // Check if user exists and password is correct
+        if (!users.isPassword(username, password)) {
+            return Response.status(401).entity("Invalid username or password.").build();
+        }
+
+        // User exists and password is correct, return user information
+        return Response.ok(username).build();
+    }
+
+    @POST
+    @Path("/create-server/{server-name}/{username}")
+    @Produces("text/plain")
+    public Response createServer(@PathParam("server-name") String name, @PathParam("username") String username) {
+        // Create a new server with the given name
+        String id = servers.createServer(name);
+        users.getUser(username).addServer(id);
+        // Return the server ID in the response body
+        return Response.ok(id).build();
+    }
+
+    @GET
+    @Path("/get-server-list/{username}")
+    @Produces("text/plain")
+    public Response getServerList(@PathParam("username") String username) {
+        String list = users.getUser(username).getServerList().toString();
+        return Response.ok(list).build();
+    }
+
+    @POST
+    @Path("/join-server/{server-id}/{username}")
+    public void joinServer(@PathParam("server-id") String id, @PathParam("username") String username) {
+        if(!servers.isServer(id)){
+            return Response.status(401).entity("Invalid server ID.").build();
+        }
+        servers.joinServer(id, username);
+        users.addServertoUser(id, username);
+    }
+
+    @GET
+    @Path("/get-server-rooms/{server-id}")
+    @Produces("text/plain")
+    public Response getServerRooms(@PathParam("server-id") String id) {
+        String list = servers.getServer(id).getRoomCodes().toString();
+        return Response.ok(list).build();
+    }
+
+    @GET
+    @Path("/get-server-name/{server-id}")
+    @Produces("text/plain")
+    public Response getServerName(@PathParam("server-id") String id) {
+        String name = servers.getServer(id).getName();
+        return Response.ok(name).build();
+    }
+
+    @GET
+    @Path("/get-server-users/{server-id}")
+    @Produces("text/plain")
+    public Response getServerUsers(@PathParam("server-id") String id) {
+        String list = servers.getServer(id).getUserList().toString();
+        return Response.ok(list).build();
+    }
+
+    @POST
+    @Path("/add-room/{server-id}")
+    public void addRoom(@PathParam("server-id") String id) {
+        servers.getServer(id).addRoomCodes(servers.generatingRandomUpperAlphanumericString());
+    }
+
     @GET
     @Produces("text/plain")
-    public String hello() {
+    @Path("/test")
+    public String test() {
         return "Hello, World!";
     }
 }
--- BisRopeServer/src/main/java/com/example/bisropeserver/util/BisropeUsers.java
+++ BisRopeServer/src/main/java/com/example/bisropeserver/util/BisropeUsers.java
@@ -6,17 +6,17 @@ import com.example.bisropeserver.domain.User;
 public class BisropeUsers {
     private static HashMap<String,User> bisropeUsers = new HashMap<>();
 
-    public User getUser(String email){
-        return bisropeUsers.get(email);
+    public User getUser(String username){
+        return bisropeUsers.get(username);
     }
 
-    public void createUser(String email, String username, String password){
-        User user = new User(username, password, email);
-        bisropeUsers.put(email,user);
+    public void createUser(String username, String password){
+        User user = new User(username, password,);
+        bisropeUsers.put(username,user);
     }
 
-    public boolean isUser(String email){
-        if(bisropeUsers.containsKey(email)){
+    public boolean isUser(String username){
+        if(bisropeUsers.containsKey(username)){
             return true;
         }
         else{
@@ -24,11 +24,21 @@ public class BisropeUsers {
         }
     }
 
-    public boolean isPassword(String email, String password){
-        if (bisropeUsers.containsKey(email)){
-            if (bisropeUsers.get(email).equals(password)){
+    public boolean isPassword(String username, String password){
+        if (bisropeUsers.containsKey(username)){
+            if (bisropeUsers.get(username).getPassword().equals(password)){
                 return true;
             }
+            else{
+                return false;
+            }
         }
+        else{
+            return false;
+        }
+    }
+
+    public void addServertoUser(String id, String username){
+        bisropeUsers.get(username).addServer(id);
     }
 }
